3678df6ed21566a4d6e6498859bd060fa94c62b4
Kirill <kirillpank2003@gmail.com>
2022-04-16 13:30:56 +0300

    commit

---
+++ Lab1/text_analyze/constants/constants.py
@@ -0,0 +1,5 @@
+SOURCE_FILE = './input.txt'
+OUTPUT_FILE = 'response.txt'
+
+DEFAULT_N = 4
+DEFAULT_K = 10
--- Lab1/text_analyze/main.py
+++ Lab1/text_analyze/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from utils.utils import read_text_from_file, get_command_line_args, write_to_file
+from constants.constants import DEFAULT_N, DEFAULT_K, SOURCE_FILE, OUTPUT_FILE
+from text_analyzer.types import Query
+from text_analyzer.analyzer import analyze_text
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    command_line_arguments = get_command_line_args((DEFAULT_N, DEFAULT_K))
+    text = read_text_from_file(SOURCE_FILE)
+    query = Query(text, *command_line_arguments)
+
+    response = analyze_text(query)
+
+    print(response)
+
+    write_to_file(OUTPUT_FILE, str(response))
+
+    cort = (5, 6, 7)
+    array = [5, 6, 7]
+    set = {5, 6,7, 8, 8, 8, 8, 10}
+    map = {'a': 5,
+           'kirill': 6,
+           'pc': [5, 6, 7],
+           5: 9}
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
